ef1c4773ee26e7c14b8a72e3438245eedba61b2e
Russell Bryant <rbryant@redhat.com>
2013-06-27 22:15:58 -0400

    combine HTML outputs for all projects.

--- genresults.sh
+++ genresults.sh
@@ -22,13 +22,17 @@ for project in ${projects} ; do
 done
 
 if [ "${all}" = "1" ] ; then
-	rm results/all-openreviews.txt
 	(date -u && echo && ./openreviews.py -a) > results/all-openreviews.txt.tmp
 	for f in results/*-openreviews.txt ; do
 		(echo && cat $f) >> results/all-openreviews.txt.tmp
 	done
 	mv results/all-openreviews.txt.tmp results/all-openreviews.txt
-	./openreviews.py -a --html > results/all-openreviews.html
+	./openreviews.py -a --html | grep -v '</html>' > results/all-openreviews.html.tmp
+	for f in results/*-openreviews.html ; do
+		cat $f | grep -v 'html>' | grep -v 'head>' >> results/all-openreviews.html.tmp
+	done
+	echo "</html>" >> results/all-openreviews.html.tmp
+	mv results/all-openreviews.html.tmp results/all-openreviews.html
 
 	for time in 30 90 180 ; do
 		(date -u && echo && ./reviewers.py -a -d ${time}) > results/all-reviewers-${time}.txt
