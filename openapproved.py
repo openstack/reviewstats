88bb5d1a42b817a000f10ec0989282c0998cc1b0
Joe Gordon <joe.gordon0@gmail.com>
2013-07-08 17:42:43 +0100

    Add approved and open
    tool to identify previously approved but open patches. Due to required rebase.
    
    Change-Id: I4b87c36e7ce077f2cec2bbeb910f0c8f7ae3d080
    Reviewed-on: https://review.openstack.org/36100
    Approved: Russell Bryant <rbryant@redhat.com>
    Reviewed-by: Russell Bryant <rbryant@redhat.com>
    Tested-by: Jenkins

---
+++ openapproved.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2011 - Soren Hansen
+# Copyright (C) 2013 - Red Hat, Inc.
+#
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Identify approved and open patches.
+
+Prints out list of approved patches that failed to merge and are currently
+still open.
+"""
+
+import optparse
+import sys
+
+import utils
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    optparser = optparse.OptionParser()
+    optparser.add_option('-p', '--project', default='projects/nova.json',
+                         help='JSON file describing the project to generate stats for')
+    optparser.add_option('-a', '--all', action='store_true',
+                         help='Generate stats across all known projects (*.json)')
+    optparser.add_option('-u', '--user', default='russellb', help='gerrit user')
+    optparser.add_option('-k', '--key', default=None, help='ssh key for gerrit')
+    optparser.add_option('-s', '--stable', action='store_true',
+                         help='Include stable branch commits')
+    options, args = optparser.parse_args()
+
+    projects = utils.get_projects_info(options.project, options.all)
+
+    if not projects:
+        print "Please specify a project."
+        sys.exit(1)
+
+    changes = utils.get_changes(projects, options.user, options.key,
+                                only_open=True)
+
+    approved_and_rebased = set()
+    for change in changes:
+        if 'rowCount' in change:
+            continue
+        if not options.stable and 'stable' in change['branch']:
+            continue
+        if change['status'] != 'NEW':
+            # Filter out WORKINPROGRESS
+            continue
+        for patch_set in change['patchSets'][:-1]:
+            if approved(patch_set) and not approved(change['patchSets'][-1]):
+                approved_and_rebased.add("%s %s" % (change['url'], change['subject']))
+
+    for x in approved_and_rebased:
+        print x
+    print "total %d" % len(approved_and_rebased)
+
+
+def approved(patch_set):
+    approvals = patch_set.get('approvals', [])
+    for review in approvals:
+        if review['type'] == 'APRV':
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    sys.exit(main())
