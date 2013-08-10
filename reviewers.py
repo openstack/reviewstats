4e893d09879e2a40c4ef78dcf8107bf1b40f6eaf
Russell Bryant <rbryant@redhat.com>
2013-08-10 10:26:42 -0400

    Refactor into a main() function
    Add a main() function.  Also only process one project's changes at a
    time to help reduce how much has to be held in memory at once.
    
    Change-Id: I6c59474da3e72e6cf21366f259173630a9a22863

--- reviewers.py
+++ reviewers.py
@@ -32,34 +32,12 @@ import sys
 import utils
 
 
-optparser = optparse.OptionParser()
-optparser.add_option('-p', '--project', default='projects/nova.json',
-        help='JSON file describing the project to generate stats for')
-optparser.add_option('-a', '--all', action='store_true',
-        help='Generate stats across all known projects (*.json)')
-optparser.add_option('-d', '--days', type='int', default=14,
-        help='Number of days to consider')
-optparser.add_option('-u', '--user', default=getpass.getuser(), help='gerrit user')
-optparser.add_option('-k', '--key', default=None, help='ssh key for gerrit')
-
-options, args = optparser.parse_args()
+def round_to_day(ts):
+    SECONDS_PER_DAY = 60*60*24
+    return (ts / (SECONDS_PER_DAY)) * SECONDS_PER_DAY
 
 
-projects = utils.get_projects_info(options.project, options.all)
-
-if not projects:
-    print "Please specify a project."
-    sys.exit(1)
-
-all_changes = utils.get_changes(projects, options.user, options.key)
-
-reviewers = {}
-
-cut_off = datetime.datetime.now() - datetime.timedelta(days=options.days)
-ts = calendar.timegm(cut_off.timetuple())
-
-
-def process_patchset(patchset, reviewers):
+def process_patchset(patchset, reviewers, ts):
     vote_types = set()
     for review in patchset.get('approvals', []):
         if review['type'] != 'CRVW':
@@ -89,49 +67,78 @@ def process_patchset(patchset, reviewers):
             reviewers[reviewer]['disagreements'] = cur + 1
 
 
-for change in all_changes:
-    for patchset in change.get('patchSets', []):
-        process_patchset(patchset, reviewers)
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
 
+    optparser = optparse.OptionParser()
+    optparser.add_option('-p', '--project', default='projects/nova.json',
+            help='JSON file describing the project to generate stats for')
+    optparser.add_option('-a', '--all', action='store_true',
+            help='Generate stats across all known projects (*.json)')
+    optparser.add_option('-d', '--days', type='int', default=14,
+            help='Number of days to consider')
+    optparser.add_option('-u', '--user', default=getpass.getuser(), help='gerrit user')
+    optparser.add_option('-k', '--key', default=None, help='ssh key for gerrit')
 
-def round_to_day(ts):
-    SECONDS_PER_DAY = 60*60*24
-    return (ts / (SECONDS_PER_DAY)) * SECONDS_PER_DAY
+    options, args = optparser.parse_args()
 
+    projects = utils.get_projects_info(options.project, options.all)
 
-reviewers = [(v, k) for k, v in reviewers.iteritems()
-             if k.lower() not in ('jenkins', 'smokestack')]
-reviewers.sort(reverse=True, key=lambda r:r[0]['total'])
+    if not projects:
+        print "Please specify a project."
+        sys.exit(1)
 
-if options.all:
-    print 'Reviews for the last %d days in projects: %s' % (options.days,
-            [project['name'] for project in projects])
-else:
-    print 'Reviews for the last %d days in %s' % (options.days, projects[0]['name'])
-if options.all:
-    print '** -- Member of at least one core reviewer team'
-else:
-    print '** -- %s-core team member' % projects[0]['name']
-table = prettytable.PrettyTable(
-        ('Reviewer', 'Reviews (-2|-1|+1|+2) (+/- ratio)', 'Disagreements'))
-total = 0
-for k, v in reviewers:
-    in_core_team = False
+    reviewers = {}
+
+    cut_off = datetime.datetime.now() - datetime.timedelta(days=options.days)
+    ts = calendar.timegm(cut_off.timetuple())
+
     for project in projects:
-        if v in project['core-team']:
-            in_core_team = True
-            break
-    name = '%s%s' % (v, ' **' if in_core_team else '')
-    plus = float(k['votes']['2'] + k['votes']['1'])
-    minus = float(k['votes']['-2'] + k['votes']['-1'])
-    ratio = (plus / (plus + minus)) * 100
-    r = '%d (%d|%d|%d|%d) (%.1f%%)' % (k['total'],
-            k['votes']['-2'], k['votes']['-1'],
-            k['votes']['1'], k['votes']['2'], ratio)
-    dratio = (float(k['disagreements']) / float(k['total'])) * 100
-    d = '%d (%.1f%%)' % (k['disagreements'], dratio)
-    table.add_row((name, r, d))
-    total += k['total']
-print table
-print '\nTotal reviews: %d' % total
-print 'Total reviewers: %d' % len(reviewers)
+        changes = utils.get_changes([project], options.user, options.key)
+        for change in changes:
+            for patchset in change.get('patchSets', []):
+                process_patchset(patchset, reviewers, ts)
+
+    reviewers = [(v, k) for k, v in reviewers.iteritems()
+                 if k.lower() not in ('jenkins', 'smokestack')]
+    reviewers.sort(reverse=True, key=lambda r:r[0]['total'])
+
+    if options.all:
+        print 'Reviews for the last %d days in projects: %s' % (options.days,
+                [project['name'] for project in projects])
+    else:
+        print 'Reviews for the last %d days in %s' % (options.days, projects[0]['name'])
+    if options.all:
+        print '** -- Member of at least one core reviewer team'
+    else:
+        print '** -- %s-core team member' % projects[0]['name']
+    table = prettytable.PrettyTable(
+            ('Reviewer', 'Reviews (-2|-1|+1|+2) (+/- ratio)', 'Disagreements'))
+    total = 0
+    for k, v in reviewers:
+        in_core_team = False
+        for project in projects:
+            if v in project['core-team']:
+                in_core_team = True
+                break
+        name = '%s%s' % (v, ' **' if in_core_team else '')
+        plus = float(k['votes']['2'] + k['votes']['1'])
+        minus = float(k['votes']['-2'] + k['votes']['-1'])
+        ratio = (plus / (plus + minus)) * 100
+        r = '%d (%d|%d|%d|%d) (%.1f%%)' % (k['total'],
+                k['votes']['-2'], k['votes']['-1'],
+                k['votes']['1'], k['votes']['2'], ratio)
+        dratio = (float(k['disagreements']) / float(k['total'])) * 100
+        d = '%d (%.1f%%)' % (k['disagreements'], dratio)
+        table.add_row((name, r, d))
+        total += k['total']
+    print table
+    print '\nTotal reviews: %d' % total
+    print 'Total reviewers: %d' % len(reviewers)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
