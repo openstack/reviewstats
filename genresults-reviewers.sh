5ca53dc5ac598bb3bb2a5c8ce2f61c0977af64d8
Robert Collins <rbtcollins@hp.com>
2013-10-08 08:06:17 +1300

    Also generate 60 day stats.
    I'd like to be able to trace a little more short term history, if
    thats ok.
    
    Change-Id: If08503e96cd5f69a4f8c150f709e6e39a9d68a18

--- genresults-reviewers.sh
+++ genresults-reviewers.sh
@@ -25,13 +25,13 @@ metadata() {
 
 for project in ${projects} ; do
 	project_base=$(basename $(echo ${project} | cut -f1 -d'.'))
-	for time in 30 90 180 ; do
+	for time in 30 60 90 180 ; do
 		(metadata && ./reviewers.py -p ${project} -d ${time} ${EXTRA_ARGS}) > results/${project_base}-reviewers-${time}.txt
 	done
 done
 
 if [ "${all}" = "1" ] ; then
-	for time in 30 90 180 ; do
+	for time in 30 60 90 180 ; do
 		(metadata && ./reviewers.py -a -d ${time} ${EXTRA_ARGS}) > results/all-reviewers-${time}.txt
 	done
 fi
