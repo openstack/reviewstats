52b26e253b2ce0b336a173c3ce7b1026cb2aa8ee
Russell Bryant <rbryant@redhat.com>
2013-11-03 13:06:52 +0800

    generate reviewers stats for 1 and 3 years
    Change-Id: Ib274166b49877838a1db20fcd84605c1bfc6b981

--- genresults-reviewers.sh
+++ genresults-reviewers.sh
@@ -25,13 +25,13 @@ metadata() {
 
 for project in ${projects} ; do
 	project_base=$(basename $(echo ${project} | cut -f1 -d'.'))
-	for time in 30 60 90 180 ; do
+	for time in 30 60 90 180 365 1095; do
 		(metadata && ./reviewers.py -p ${project} -d ${time} ${EXTRA_ARGS} -o results/${project_base}-reviewers-${time} --outputs txt --outputs csv)
 	done
 done
 
 if [ "${all}" = "1" ] ; then
-	for time in 30 60 90 180 ; do
+	for time in 30 60 90 180 365 1095; do
 		(metadata && ./reviewers.py -a -d ${time} ${EXTRA_ARGS} -o results/all-reviewers-${time} --outputs txt --outputs csv)
 	done
 fi
