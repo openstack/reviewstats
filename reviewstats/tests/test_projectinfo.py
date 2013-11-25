44fdb1f2c48d59bd06f7a987b773cb419c501e66
Robert Collins <rbtcollins@hp.com>
2013-11-25 06:56:31 +0000

    Move tests into the setuptools structure.
    Change-Id: I0bcdc59b9c23270b0742c3772ef01aaa73893bcd

--- reviewstats/tests/test_projectinfo.py
+++ reviewstats/tests/test_projectinfo.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -12,17 +10,11 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-"""
-test_reviewstats
-----------------------------------
-
-Tests for `reviewstats` module.
-"""
-
 from reviewstats.tests import base
+import utils
 
 
-class TestReviewstats(base.TestCase):
+class TestProjectInfo(base.TestCase):
 
-    def test_something(self):
-        pass
+    def test_project_definitions_load(self):
+        utils.get_projects_info('', True)
